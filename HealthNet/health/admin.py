b6779b23e33d0e889bce1673708538c332ff2d94
Harlan Haskins <harlan@harlanhaskins.com>
2015-03-01 20:43:06 +0000

    Added base HTML template

--- HealthNet/health/admin.py
+++ HealthNet/health/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import *
 
+
+class InsuranceAdmin(admin.ModelAdmin):
+    fields = ['policy_number', 'company']
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(Appointment)
 admin.site.register(Prescription)
-admin.site.register(Insurance)
+admin.site.register(Insurance, InsuranceAdmin)
 admin.site.register(Unit)
