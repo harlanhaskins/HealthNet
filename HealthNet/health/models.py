dab689e84a630507889e5d258f92151cc05c2436
Harlan Haskins <harlan@harlanhaskins.com>
2015-03-01 17:59:42 +0000

    Added models for some more parts of the UML.

--- HealthNet/health/models.py
+++ HealthNet/health/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 from django.contrib.auth import models as auth_models
 
-# Create your models here.
-
 
 class User(auth_models.User):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    date_of_birth = models.DateField()
+    schedule = models.ManyToManyField(Appointment)
+
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+
+class Insurance(models.Model):
+    policy_number = models.CharField()
+    company = models.CharField()
+
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(User)
+    doctor = models.ForeignKey(User)
+
+
+class Prescription(models.Model):
+    name = models.CharField()
+    dosage = models.FloatField()
+    unit = models.ForeignKey(Unit)
+
+
+class Unit(models.Model):
+    name = models.CharField()
+    abbreviation = models.CharField()
