38798d9d2f1bdb4d253825673ed54b1a316bf751
Harlan Haskins <harlan@harlanhaskins.com>
2015-03-01 18:22:00 +0000

    Fixed auth_user_model

--- HealthNet/HealthNet/settings.py
+++ HealthNet/HealthNet/settings.py
@@ -76,7 +76,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-AUTH_USER_MODEL = 'health.models.User'
+AUTH_USER_MODEL = 'health.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
