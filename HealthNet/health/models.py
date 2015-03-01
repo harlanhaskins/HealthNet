f6855a4dec5ef75f75c1a0ddaba7c33ee3bca4cd
Harlan Haskins <harlan@harlanhaskins.com>
2015-03-01 18:12:39 +0000

    Fixed build errors.

--- HealthNet/health/models.py
+++ HealthNet/health/models.py
@@ -3,31 +3,28 @@ from django.contrib.auth import models as auth_models
 
 
 class User(auth_models.User):
-    first_name = models.CharField()
-    last_name = models.CharField()
     date_of_birth = models.DateField()
-    schedule = models.ManyToManyField(Appointment)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
 
 class Insurance(models.Model):
-    policy_number = models.CharField()
-    company = models.CharField()
+    policy_number = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
 
 
 class Appointment(models.Model):
-    patient = models.ForeignKey(User)
-    doctor = models.ForeignKey(User)
-
-
-class Prescription(models.Model):
-    name = models.CharField()
-    dosage = models.FloatField()
-    unit = models.ForeignKey(Unit)
+    patient = models.ForeignKey(User, related_name="User")
+    doctor = models.ForeignKey(User, related_name="Doctor")
 
 
 class Unit(models.Model):
-    name = models.CharField()
-    abbreviation = models.CharField()
+    name = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=200)
+
+
+class Prescription(models.Model):
+    name = models.CharField(max_length=200)
+    dosage = models.FloatField()
+    unit = models.ForeignKey(Unit)
