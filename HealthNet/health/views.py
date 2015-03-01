e4d6943eeec2e20223affdceadaa0e5bf0207dfc
Harlan Haskins <harlan@harlanhaskins.com>
2015-03-01 21:07:56 +0000

    Added index.html and login/index views

--- HealthNet/health/views.py
+++ HealthNet/health/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def index(request):
+    return render(request, 'index.html', {"user":request.user})
+
+def login(request):
+    return render(request, 'login.html', None)
