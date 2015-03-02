107f4439ca4b2e7821ee0b3a0a914968f9d9634a
Harlan Haskins <harlan@harlanhaskins.com>
2015-03-02 15:51:48 +0000

    Added prescriptions endpoint.

--- HealthNet/health/urls.py
+++ HealthNet/health/urls.py
@@ -3,8 +3,11 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = patterns('',
-    url(r'login/?$', auth_views.login, name='login'),
+    url(r'login/?$', views.login_view, name='login'),
+    url(r'logout/?$', views.logout_view, name='logout'),
     url(r'schedule/?$', views.schedule, name='schedule'),
+    url(r'prescriptions/?$', views.schedule, name='prescriptions'),
     url(r'signup/?$', views.signup, name='signup'),
     url(r'/?$', views.index, name='index'),
 )
+
--- HealthNet/health/views.py
+++ HealthNet/health/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, RequestContext
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -7,8 +8,28 @@ def index(request):
 
 
 def signup(request):
-    return render(request, 'signup.html', None)
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+    if request.POST:
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('health:index')
+    return render(request, 'registration/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('health:index')
+
+@login_required
+def prescriptions(request):
+    return render(request, 'prescriptions.html')
 
 @login_required
 def schedule(request):
-    return render(request, 'schedule.html', None)
+    return render(request, 'schedule.html')
