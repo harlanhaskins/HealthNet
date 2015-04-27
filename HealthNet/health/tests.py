ae47877644cdd0bb112a6ee3e23fb5ed56ef5305
Harlan Haskins <harlan@harlanhaskins.com>
2015-04-27 22:30:48 +0000

    Fixed tests.

--- HealthNet/health/tests.py
+++ HealthNet/health/tests.py
@@ -6,57 +6,64 @@ from .models import *
 class UserTestCase(TestCase):
 
     def setUp(self):
-        self.hospital = Hospital.objects.create(name="University of Rochester Medical Center",
-                                                address="601 Elmwood Ave", state="New York", city="Rochester",
-                                                zipcode="14620")
+        """
+        Creates many users in the database.
+        """
+        h = Hospital.objects.create(name="University of Rochester Medical Center",
+                                    address="601 Elmwood Ave", state="New York", city="Rochester",
+                                    zipcode="14620")
 
-        self.hospital2 = Hospital.objects.create(name="RIT Health Center",
-                                                 address="1 Lomb Memorial Drive", state="New York", city="Rochester",
-                                                 zipcode="14623")
+        h2 = Hospital.objects.create(name="RIT Health Center",
+                                    address="1 Lomb Memorial Drive", state="New York", city="Rochester",
+                                    zipcode="14623")
 
-        self.hospital3 = Hospital.objects.create(name="Highland Hospital",
-                                                 address="1000 South Ave", state="New York", city="Rochester",
-                                                 zipcode="14620")
+        h3 = Hospital.objects.create(name="Highland Hospital",
+                                     address="1000 South Ave", state="New York", city="Rochester",
+                                     zipcode="14620")
 
-        self.patients = Group.objects.create(name="Patient")
-        self.doctors = Group.objects.create(name="Doctor")
-        self.nurses = Group.objects.create(name="Nurse")
+        patients = Group.objects.create(name="Patient")
+        doctors = Group.objects.create(name="Doctor")
+        nurses = Group.objects.create(name="Nurse")
 
         email = "admin@djangomaintained.com"
-        self.admin = User.objects.create_superuser('admin', email=email, first_name="Administrator",
-                                                   last_name="Jones", password="SuperSecurePassword1234", phone_number="8649189255",
-                                                   hospital=self.hospital, date_of_birth=datetime.date(year=1995, month=4, day=27))
+        admin = User.objects.create_superuser('admin', email=email, first_name="Administrator",
+                last_name="Jones", password="SuperSecurePassword1234", phone_number="8649189255",
+                hospital=h, date_of_birth=datetime.date(year=1995, month=4, day=27))
 
         email = "jd@sacredheart.org"
-        self.doctor = User.objects.create_user(email, email=email, first_name="John",
-                                               last_name="Dorian", password="SuperSecurePassword1234", phone_number="18005553333",
-                                               hospital=self.hospital, date_of_birth=datetime.date(year=1980, month=6, day=7))
-        self.doctors.user_set.add(self.doctor)
+        doctor = User.objects.create_user(email, email=email, first_name="John",
+                 last_name="Dorian", password="SuperSecurePassword1234", phone_number="18005553333",
+                 hospital=h, date_of_birth=datetime.date(year=1980, month=6, day=7))
+        doctors.user_set.add(doctor)
         email = "turk@sacredheart.org"
-        self.doctor2 = User.objects.create_user(email, email=email, first_name="Christopher",
-                                                last_name="Turkleton", password="SuperSecurePassword1234", phone_number="18005553333",
-                                                hospital=self.hospital, date_of_birth=datetime.date(year=1980, month=6, day=7))
-        self.doctors.user_set.add(self.doctor2)
+        doctor = User.objects.create_user(email, email=email, first_name="Christopher",
+                 last_name="Turkleton", password="SuperSecurePassword1234", phone_number="18005553333",
+                 hospital=h, date_of_birth=datetime.date(year=1980, month=6, day=7))
+        doctors.user_set.add(doctor)
         email = "drcox@sacredheart.org"
-        self.doctor3 = User.objects.create_user(email, email=email, first_name="Perry",
-                                                last_name="Cox", password="SuperSecurePassword1234", phone_number="18005553333",
-                                                hospital=self.hospital, date_of_birth=datetime.date(year=1980, month=6, day=7))
-        self.doctors.user_set.add(self.doctor3)
+        doctor = User.objects.create_user(email, email=email, first_name="Perry",
+                 last_name="Cox", password="SuperSecurePassword1234", phone_number="18005553333",
+                 hospital=h, date_of_birth=datetime.date(year=1980, month=6, day=7))
+        doctors.user_set.add(doctor)
 
         email = "carla@sacredheart.org"
-        self.nurse = User.objects.create_user(email, email=email, first_name="Carla",
-                                              last_name="Turkleton", password="SuperSecurePassword1234", phone_number="18005553333",
-                                              hospital=self.hospital, date_of_birth=datetime.date(year=1976, month=3, day=9))
-        self.nurses.user_set.add(self.nurse)
+        nurse = User.objects.create_user(email, email=email, first_name="Carla",
+                last_name="Turkleton", password="SuperSecurePassword1234", phone_number="18005553333",
+                hospital=h, date_of_birth=datetime.date(year=1976, month=3, day=9))
+        nurses.user_set.add(nurse)
 
-        self.insurance = Insurance.objects.create(company="Hobo Sal's Used Needle Emporium",
-                                                  policy_number="8675309")
+        insurance = Insurance.objects.create(company="Hobo Sal's Used Needle Emporium",
+                                             policy_number="8675309")
+
+        medical_info = MedicalInformation.objects.create(sex='Male', insurance=insurance, medications=None,
+                                                         allergies=None, medical_conditions="Brain Tumor",
+                                                         family_history=None, additional_info="Oh, you guys!")
         email = "duwayne@theroc-johnson.com"
-        self.patient = User.objects.create_user(email, email=email, first_name="Duwayne",
-                                                last_name="Theroc-Johnson", password="SuperSecurePassword1234", phone_number="18005553333",
-                                                hospital=self.hospital, date_of_birth=datetime.date(year=1991, month=3, day=29), insurance=self.insurance)
+        patient = User.objects.create_user(email, email=email, first_name="Duwayne",
+                  last_name="Theroc-Johnson", password="SuperSecurePassword1234", phone_number="18005553333",
+                  hospital=h, date_of_birth=datetime.date(year=1991, month=3, day=29), medical_information=medical_info)
 
-        self.patients.user_set.add(self.patient)
+        patients.user_set.add(patient)
 
     def test_group_definitions(self):
         self.assertTrue(self.patient.is_patient())
